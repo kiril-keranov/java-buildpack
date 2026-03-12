93dafec59d22275b15cb38f522bdb90699b06a70
Kiril Keranov <k.keranov@sap.com>
2026-03-12 12:11:33 +0200

    Try changes

--- src/java/containers/tomcat.go
+++ src/java/containers/tomcat.go
@@ -259,7 +259,7 @@ func (t *TomcatContainer) createSetenvScript(tomcatDir, loggingSupportJar string
 	jarPath := "$CATALINA_HOME/bin/" + loggingSupportJar
 
 	setenvContent := fmt.Sprintf(`#!/bin/sh
-CLASSPATH=""$CLASSPATH:%s"
+CLASSPATH="$CLASSPATH:%s"
 echo "Testing check $JAVA_OPTS"
 echo "Testing check1 $CLASSPATH"
 `, jarPath)
